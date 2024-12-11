f15c8547d7e8cfb52d2ff86b2b28ea028db521b7
DaniloAlvesantos <daniloasan.itapira@gmail.com>
2024-12-11 13:30:26 -0300

    fixing table mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
 import { ReactQueryProvider } from "@/context/query";
